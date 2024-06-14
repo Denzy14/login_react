902a6148b84012dcd62d45fa721a3d2cb75affae
Denzy14 <denzelbj@stpeter.com.ph>
2024-06-14 09:31:10 +0800

    PRODUCTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import ProductList from "./components/ProductList";
 
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
+
 function App() {
   const [category, setCategory] = useState("");
 
   useEffect(() => {
-    document.title = "My App";
+    connect();
+
+    return () => disconnect();
   });
 
   return (
@@ -22,5 +27,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
